2a191b3f8a34b3093235ab93e224268baa0d6660
jritter <jaronritter01@gmail.com>
2024-09-16 23:04:57 -0500

    remove exit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jaron/CLionProjects/chip8
+# Install script for directory: C:/Users/jaron/OneDrive/Documents/projects/chip8
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/jaron/CLionProjects/chip8/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jaron/OneDrive/Documents/projects/chip8/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
